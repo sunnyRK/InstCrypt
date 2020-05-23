840aedbc0344330a00744f81c9a84c34cc8f8981
sunnyRK <radadiyasunny970@gmail.com.com>
2020-05-23 08:46:39 +0530

    Added Support for heroku deployment

--- node-api/models/ProgramModel.js
+++ node-api/models/ProgramModel.js
@@ -1,71 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create test Schema and model 
-
-const ProgramSchema = new Schema({
-    programName:{
-        type: String,
-        required: [true, 'Program Name field is required']
-    },
-    programPurpose:{
-        type: String,
-        required: [true, 'Program Purpose field is required']
-    },
-    programCategory:{
-        type: String,
-        required: [true, 'Program Category field is required']
-    },
-    finetsType:{
-        type: Number,
-        required: [true, 'Program Category field is required']
-    },
-    numberOfParticipants:{
-        type: Number,
-        required: [true, 'Number of Participants field is required']
-    },
-    installmentEachRound: {
-        type: Number,
-        required: [true, 'Installment Each Round field is required']
-    },
-    programOwner: {
-        type: String,
-        required: [true, 'Program Owner field is required']
-    },
-    startDate:{
-        type: String,
-        required: [true, 'Start Date field is required']
-    },
-    dateOfCreation: {
-        type: String,
-        required: [true, 'Date of Creation field is required']
-    },
-    program: {
-        type:String,
-        required: [true, 'Program field is required']
-    },
-    ProgramStatus: {
-        type: Number,
-        required: [true, 'Program Status field is required']
-    },
-    sector: {
-        type: Number,
-        required: [true, 'Sector field is required']
-    },
-    STQScore: {
-        type: Number,
-        required: [true, 'STQ score field is required']
-    }
-});
-
-
-const ProgramSchema2 = new Schema({
-    programName:{
-        type: String,
-        required: [true, 'Program Name field is required']
-    },
-});
-
 const TransactionSchema = new Schema({
     transactionHash:{
         type: String,
@@ -93,22 +28,5 @@ const TransactionSchema = new Schema({
     },
 });
 
-
 const Program = mongoose.model("TransactionsDatabase",TransactionSchema);
 module.exports = Program;
-
-// {
-// 	"programName": "Test_1",
-// 	"programPurpose": "Test Purpose",
-// 	"programCategory": "Travel",
-// 	"finetsType": "1",
-// 	"numberOfParticipants": "5",
-// 	"installmentEachRound": "100",
-// 	"programOwner": "0x00000000",
-// 	"stratDate": "15511111",
-// 	"dateOfCreation": "1522222",
-// 	"program": "progs1",
-// 	"ProgramStatus": "1",
-// 	"sector": "2",
-// 	"STQScore": "123"
-// }
