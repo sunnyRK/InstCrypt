8c217dd3f3fa236fb41b8f2c7a2132d90c0f4405
sunnyRK <radadiyasunny970@gmail.com.com>
2020-05-23 10:08:45 +0530

    change port number

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const routes = require("./routes");
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(3000,(err) => {
+    createServer(handler).listen(1923,(err) => {
         if(err) throw err;
-        console.log("Ready on localhost:3000");
+        console.log("Ready on localhost:1923");
     });
 });
